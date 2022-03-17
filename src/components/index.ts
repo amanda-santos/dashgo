cd7c5f71400a089be1575ca0cb0abe16b0b6cab4
amanda-santos <amandasf0200@gmail.com>
2022-03-17 18:52:41 -0300

    ✨ Add Sidebar

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export { Header } from "./Header";
 export { Input } from "./Form/Input";
+export { Sidebar } from "./Sidebar";
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,9 +1,13 @@
-import { Header } from "../components";
+import { Flex } from "@chakra-ui/react";
+import { Header, Sidebar } from "../components";
 
 export default function Dashboard() {
   return (
-    <>
-      <Header></Header>
-    </>
+    <Flex direction="column" h="100vh">
+      <Header />
+      <Flex w="100%" my="6" maxWidth={1400} mx="auto" px="6">
+        <Sidebar />
+      </Flex>
+    </Flex>
   );
 }
